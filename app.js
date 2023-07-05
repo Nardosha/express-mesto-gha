a5fc33249b0dae6baf1dda709a32ca0cbfab5a2b
natali <NataliDegtyreva@yandex.ru>
2023-07-06 01:05:11 +0300

    feat: add last routing handler

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import bodyParser from "body-parser";
 import usersRoutes from "./routes/users.js"
 import cardRoutes from "./routes/cards.js"
 
+import {NOT_FOUND_ERROR_CODE} from "./utils/ENUMS.js";
+
 const { PORT = 3000 } = process.env;
 
 mongosse.connect('mongodb://localhost:27017/mestodb');
@@ -23,6 +25,10 @@ app.use((req, res, next) => {
 
 app.use('/users', usersRoutes)
 app.use('/cards', cardRoutes)
+
+app.use((req, res) => {
+  res.status(NOT_FOUND_ERROR_CODE).send({message: "Запрашиваемой страницы не существует"})
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`)
